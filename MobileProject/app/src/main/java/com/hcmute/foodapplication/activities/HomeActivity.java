4f277707b246272ea2357cae231d7711b81b2fd8
KhoaDangPhamVu <116179021+KhoaDangPhamVu@users.noreply.github.com>
2023-02-06 20:21:25 +0700

    LabWork2
    LabWork2 first project apply 23 attributes Done 6/2/2023

---
+++ MobileProject/app/src/main/java/com/hcmute/foodapplication/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.hcmute.foodapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hcmute.foodapplication.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
